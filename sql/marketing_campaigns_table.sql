2570f603cbeccb95c563420d6cc4ed45a477c1a9
Sean Atkinson <99100196+sean-atkinson@users.noreply.github.com>
2023-08-04 11:24:29 -0400

    Updating formatting

--- sql/marketing_campaigns_table.sql
+++ sql/marketing_campaigns_table.sql
@@ -1,44 +1,51 @@
--- cte to calculate number of signups per campaign
+-- CTE to calculate number of signups per campaign
 WITH signup_count AS (
-  SELECT customers.campaign_id, 
-    COUNT(DISTINCT customers.customer_id) AS num_signups
-  FROM `psychic-raceway-393323.rowhealth.customers` customers 
-  GROUP BY 1
+    SELECT 
+        customers.campaign_id, 
+        COUNT(DISTINCT customers.customer_id) AS num_signups
+    FROM 
+        `psychic-raceway-393323.rowhealth.customers` customers 
+    GROUP BY 
+        customers.campaign_id
 )
--- main query to join campaigns with signup count and calculate additional metrics
--- cocatenating campaign details in a single column
--- returning null for signup_rate, cost_per_signup, click_through_rate, and cost_per_click to avoid division by zero
-SELECT campaigns.campaign_id,
-  campaigns.campaign_category,
-  campaigns.campaign_type,
-  campaigns.platform,
-  CONCAT(
-    campaigns.campaign_category, ' - ', 
-    campaigns.campaign_type, ' - ',
-    campaigns.platform
-  ) AS campaign_detail,
-  campaigns.days_run,
-  campaigns.cost,
-  campaigns.impressions,
-  campaigns.clicks,
-  signup_count.num_signups,
-  CASE
-    WHEN signup_count.num_signups = 0 THEN NULL 
-    ELSE (signup_count.num_signups / campaigns.impressions)
-  END AS signup_rate,
-  CASE
-    WHEN signup_count.num_signups = 0 THEN NULL
-    ELSE (campaigns.cost / signup_count.num_signups)
-  END AS cost_per_signup,
-  CASE
-    WHEN campaigns.impressions = 0 THEN NULL
-    ELSE (campaigns.clicks / campaigns.impressions)
-  END AS click_through_rate,
-  CASE
-    WHEN campaigns.clicks = 0 THEN NULL
-    ELSE (campaigns.cost / campaigns.clicks)
-  END AS cost_per_clicK
-FROM `psychic-raceway-393323.rowhealth.campaigns` campaigns 
-LEFT JOIN signup_count
-  ON campaigns.campaign_id = signup_count.campaign_id
-ORDER BY 1
+
+-- Main query to join campaigns with signup count and calculate additional metrics
+-- Concatenating campaign details in a single column
+-- Returning null for signup_rate, cost_per_signup, click_through_rate, and cost_per_click to avoid division by zero
+SELECT 
+    campaigns.campaign_id,
+    campaigns.campaign_category,
+    campaigns.campaign_type,
+    campaigns.platform,
+    CONCAT(
+        campaigns.campaign_category, ' - ', 
+        campaigns.campaign_type, ' - ',
+        campaigns.platform
+    ) AS campaign_detail,
+    campaigns.days_run,
+    campaigns.cost,
+    campaigns.impressions,
+    campaigns.clicks,
+    signup_count.num_signups,
+    CASE
+        WHEN signup_count.num_signups = 0 THEN NULL 
+        ELSE (signup_count.num_signups / campaigns.impressions)
+    END AS signup_rate,
+    CASE
+        WHEN signup_count.num_signups = 0 THEN NULL
+        ELSE (campaigns.cost / signup_count.num_signups)
+    END AS cost_per_signup,
+    CASE
+        WHEN campaigns.impressions = 0 THEN NULL
+        ELSE (campaigns.clicks / campaigns.impressions)
+    END AS click_through_rate,
+    CASE
+        WHEN campaigns.clicks = 0 THEN NULL
+        ELSE (campaigns.cost / campaigns.clicks)
+    END AS cost_per_click
+FROM 
+    `psychic-raceway-393323.rowhealth.campaigns` campaigns 
+LEFT JOIN 
+    signup_count ON campaigns.campaign_id = signup_count.campaign_id
+ORDER BY 
+    campaigns.campaign_id
